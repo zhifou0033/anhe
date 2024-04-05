e78410a3687bc421d3a01dabc3b6db8d5eaf5a5d
yongzhangli <1379769649@qq.com>
2024-04-05 20:01:33 +0800

    vitest配置文件添加

---
+++ vitest.config.ts
@@ -0,0 +1,21 @@
+import { resolve,dirname } from 'node:path'
+import { defineConfig } from 'vitest/config'
+import { fileURLToPath } from 'node:url';
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+export default defineConfig({
+    resolve: {
+        alias: {
+            '@rarelyjs/core': resolve(__dirname, 'packages/core/index.ts'),
+        },
+    },
+    define: {
+        __VUE_OPTIONS_API__: 'true',
+        __VUE_PROD_DEVTOOLS__: 'false',
+    },
+    test: {
+        environment: 'jsdom',
+        setupFiles: [resolve(__dirname, 'packages/.test/setup.ts')],
+        reporters: 'dot',
+    },
+})
